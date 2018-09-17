1483a10c93ed4ff215acaed8f7d0865a2bcf0681
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2018-09-17 14:52:46 +0300

    added complete methods from ping.h

--- app/ping/ping.cpp
+++ app/ping/ping.cpp
@@ -2,12 +2,57 @@
 #include <fstream>
 #include "ping.h"
 
-// Chunk structure constuctor
-ping::Chunk( 
-		unsigned long int getSize,
-		const char* getName
+// Constructor
+ping::Chunk::Chunk( 
+		unsigned long getSize,
+		const char * getName
 		) 
-	{
-		size = getSize;
-		name = getName;
+{
+	size = getSize;
+	name = getName;
+	content = new unsigned char[size];
+}
+
+// Getters
+unsigned long ping::Chunk::getCrc32()
+{
+	return crc32;
+}
+
+unsigned long ping::Chunk::getSizeInt()
+{
+	return size;
+}
+
+std::string ping::Chunk::getSizeChar() // Convert integer size to file-ready char array
+{
+	std::stringstream stream;
+	stream << "0x" << std::setfill ('0') << std::setw(sizeof(unsigned long)*2) << std::hex << size;
+	std::string str( stream.str() );
+		
+	return str;
+}
+
+const char* ping::Chunk::getName() //test for chunk name
+{
+	return name;
+}
+
+// Setters
+void ping::Chunk::setCrc32(unsigned long arg)
+{
+	crc32 = arg;
+}
+
+//CRC32 calc
+unsigned long ping::calcCrc(unsigned long *CrcTable, unsigned long crc, unsigned char *buf, int len)
+{
+	unsigned long crc32 = crc;
+	int n;
+
+	for (n = 0; n < len; n++) {
+		crc32 = CrcTable[(crc32 ^ buf[n]) & 0xff] ^ (crc32 >> 8);
 	}
+
+	return crc32 ^ 0xffffffffL;
+}
