6a3a7119a3cbd43a83d19aa2a17dc543f6161b3a
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2018-09-17 14:53:26 +0300

    updated to work with ping

--- app/ping/main.cpp
+++ app/ping/main.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 #include "ping.h"
 
+void MakeCrcTable();
+
+//Table of CRCs of all 8-bit messages.
+unsigned long crc_table[256];
+
 /*struct Ping //PNG structure
 {
 	const unsigned char header[] = { 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, '\0'};
@@ -18,17 +23,39 @@ int main()
 	ping::Chunk** chunk = new ping::Chunk*[1]; //array of chunks
 	chunk[0] = new ping::IHDR();
 	std::cout << "hi"; //debug
-	std::cout << chunk[0]->getSize_char(); //debug
+	std::cout << chunk[0]->getSizeInt(); //debug
 	
 	const unsigned char header[] = { 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a, '\0'};
 	
 	//std::cout << header; //debug
 	
+	MakeCrcTable();
+	//make ihdr crc
+	chunk[0]->setCrc32(ping::calcCrc( crc_table, 0xffffffffL, chunk[0]->getName(), (chunk[0]->getSizeInt())-9));
+	
 	std::ofstream file;
 	file.open("test.png", std::ios::binary );
-	file << header << chunk[0]->getN();
-	file.write(chunk[0]->getSize_char(), sizeof(char)*chunk[0]->getSize_char().size());
+	file << header << chunk[0]->getName() << chunk[0]->getSizeChar() << chunk[0]->getCrc32();
+	//file.write(chunk[0]->getSize_char(), sizeof(char)*chunk[0]->getSize_char().size());
 	file.close();
 	
 	return 0;	
+}
+
+// Make the table for a fast CRC.
+void MakeCrcTable()
+{
+	unsigned long c;
+	int n, k;
+	
+	for (n = 0; n < 256; n++) {
+		c = (unsigned long) n;
+		for (k = 0; k < 8; k++) {
+		if (c & 1)
+			c = 0xedb88320L ^ (c >> 1);
+		else
+			c = c >> 1;
+		}
+		crc_table[n] = c;
+	}
 }
